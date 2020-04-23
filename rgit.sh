42a8c83a972ae70eeed9390ab50c31c10f0a4a80
Marco Milon <marco.milon@gmail.com>
2020-04-23 17:55:53 -0500

    Delete unneeded files

--- rgit.sh
+++
@@ -1,5 +0,0 @@
-git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "marco@ctdevelopers.com" ]; then
-     GIT_AUTHOR_EMAIL=marco.milon@gmail.com;
-     GIT_AUTHOR_NAME="Marco Milon";
-     GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
-     GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
