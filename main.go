f8795a6ee7a8e5c528190e3cea28aaf2e5b2c6cb
Marco Milon <marco.milon@gmail.com>
2020-08-30 12:38:17 -0500

    Add main.go file

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/marcomilon/gstatic/internal/datasource"
+	"github.com/marcomilon/gstatic/internal/generator"
 )
 
 func main() {
 	fmt.Println("gStatic")
+
+	argsWithoutProg := os.Args[1:]
+
+	srcFolder := argsWithoutProg[0]
+	targetFolder := argsWithoutProg[1]
+
+	ds := datasource.Yaml{}
+
+	yamlGen := generator.Generator{ds}
+
+	err := yamlGen.Generate(srcFolder, targetFolder)
+	if err != nil {
+		fmt.Printf("expected %v; got %v", nil, err)
+	}
+
 }
